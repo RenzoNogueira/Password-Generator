c84f3f16cd6d27bd2675dc974e9c9ea16884e42d
RenzoNogueira <nogueirarenzoso@gmail.com>
2020-08-25 11:00:34 -0300

    Função que verifica a força da senha atualizada

--- js/script.js
+++ js/script.js
@@ -70,7 +70,7 @@ function generatePassword() {
             for (let i = 0; i < CUSTOM_TEXT.length; i++) {
                 password.text = password.text.replace(password.text[Math.floor(Math.random() *
                     (password.text.length))], (Math.floor(Math.random() * (1.1)) === 0 ? CUSTOM_TEXT[Math.floor(Math.random() *
-                        (CUSTOM_TEXT.length))] : CUSTOM_TEXT[Math.floor(Math.random() * (CUSTOM_TEXT.length))].toUpperCase()))
+                    (CUSTOM_TEXT.length))] : CUSTOM_TEXT[Math.floor(Math.random() * (CUSTOM_TEXT.length))].toUpperCase()))
             }
             password.text = password.text.replace(/ /g, "_")
         }
@@ -92,35 +92,127 @@ function generatePassword() {
 }
 
 function checkPasswordStrength(senha) {
-    let forca = 0;
-    let mostra_res
-    if ((senha.length >= 4) && (senha.length <= 7)) {
-        forca += 10;
-    } else if (senha.length > 7) {
-        forca += 25;
-    }
-    if (senha.match(/[a-z]+/)) {
-        forca += 10;
-    }
-    if (senha.match(/[A-Z]+/)) {
-        forca += 20;
-    }
-    if (senha.match(/d+/)) {
-        forca += 20;
-    }
-    if (senha.match(/W+/)) {
-        forca += 25;
-    }
+    // let forca = 0;
+    // let mostra_res
+    // if ((senha.length >= 4) && (senha.length <= 7)) {
+    //     forca += 10;
+    // } else if (senha.length > 7) {
+    //     forca += 25;
+    // }
+    // if (senha.match(/[a-z]+/)) {
+    //     forca += 10;
+    // }
+    // if (senha.match(/[A-Z]+/)) {
+    //     forca += 20;
+    // }
+    // if (senha.match(/d+/)) {
+    //     forca += 20;
+    // }
+    // if (senha.match(/W+/)) {
+    //     forca += 25;
+    // }
 
-    if (forca < 30) {
-        mostra_res = "Weak"
-    } else if ((forca >= 30) && (forca < 60)) {
-        mostra_res = "Good"
-    } else if ((forca >= 60) && (forca < 85)) {
-        mostra_res = "Strong"
+    // if (forca < 30) {
+    //     mostra_res = "Weak"
+    // } else if ((forca >= 30) && (forca < 60)) {
+    //     mostra_res = "Good"
+    // } else if ((forca >= 60) && (forca < 85)) {
+    //     mostra_res = "Strong"
+    // } else {
+    //     mostra_res = "Great"
+    // }
+
+    // return mostra_res;
+
+    var strPassword = senha;
+    var charPassword = 0;
+    var complexity = null;
+    var minPasswordLength = 8;
+    var baseScore = 0,
+        score = 0;
+
+    var num = {};
+    num.Excess = 0;
+    num.Upper = 0;
+    num.Numbers = 0;
+    num.Symbols = 0;
+
+    var bonus = {};
+    bonus.Excess = 3;
+    bonus.Upper = 4;
+    bonus.Numbers = 5;
+    bonus.Symbols = 5;
+    bonus.Combo = 0;
+    bonus.FlatLower = 0;
+    bonus.FlatNumber = 0;
+
+    if (charPassword.length >= minPasswordLength) {
+        baseScore = 50;
+        analyzeString();
+        calcComplexity();
     } else {
-        mostra_res = "Great"
+        baseScore = 0;
     }
 
-    return mostra_res;
+    charPassword = strPassword.split("");
+
+    num.Excess = 0;
+    num.Upper = 0;
+    num.Numbers = 0;
+    num.Symbols = 0;
+    bonus.Combo = 0;
+    bonus.FlatLower = 0;
+    bonus.FlatNumber = 0;
+    baseScore = 0;
+    score = 0;
+
+    for (i = 0; i < charPassword.length; i++) {
+        if (charPassword[i].match(/[A-Z]/g)) {
+            num.Upper++;
+        }
+        if (charPassword[i].match(/[0-9]/g)) {
+            num.Numbers++;
+        }
+        if (charPassword[i].match(/(.*[!,@,#,$,%,^,&,*,?,_,~])/)) {
+            num.Symbols++;
+        }
+    }
+
+    num.Excess = charPassword.length - minPasswordLength;
+
+    if (num.Upper && num.Numbers && num.Symbols) {
+        bonus.Combo = 25;
+    } else if ((num.Upper && num.Numbers) || (num.Upper && num.Symbols) || (num.Numbers && num.Symbols)) {
+        bonus.Combo = 15;
+    }
+
+    if (strPassword.match(/^[\sa-z]+$/)) {
+        bonus.FlatLower = -15;
+    }
+
+    if (strPassword.match(/^[\s0-9]+$/)) {
+        bonus.FlatNumber = -35;
+    }
+
+    score = baseScore + (num.Excess * bonus.Excess) + (num.Upper * bonus.Upper) + (num.Numbers * bonus.Numbers) + (num.Symbols * bonus.Symbols) + bonus.Combo + bonus.FlatLower + bonus.FlatNumber;
+
+    if (score < 50) {
+        return "Weak";
+    } else if (score >= 50 && score < 75) {
+        return "Medium";
+    } else if (score >= 75 && score < 100) {
+        return "Strong";
+    } else if (score >= 100) {
+        return "Safe";
+    }
+
+    // $("#details").html("Base Score :<span class=\"value\">" + baseScore + "</span>" +
+    //     "<br />Length Bonus :<span class=\"value\">" + (num.Excess * bonus.Excess) + " [" + num.Excess + "x" + bonus.Excess + "]</span> " +
+    //     "<br />Upper case bonus :<span class=\"value\">" + (num.Upper * bonus.Upper) + " [" + num.Upper + "x" + bonus.Upper + "]</span> " +
+    //     "<br />Number Bonus :<span class=\"value\"> " + (num.Numbers * bonus.Numbers) + " [" + num.Numbers + "x" + bonus.Numbers + "]</span>" +
+    //     "<br />Symbol Bonus :<span class=\"value\"> " + (num.Symbols * bonus.Symbols) + " [" + num.Symbols + "x" + bonus.Symbols + "]</span>" +
+    //     "<br />Combination Bonus :<span class=\"value\"> " + bonus.Combo + "</span>" +
+    //     "<br />Lower case only penalty :<span class=\"value\"> " + bonus.FlatLower + "</span>" +
+    //     "<br />Numbers only penalty :<span class=\"value\"> " + bonus.FlatNumber + "</span>" +
+    //     "<br />Total Score:<span class=\"value\"> " + score + "</span>");
 }
